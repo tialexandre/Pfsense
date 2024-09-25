38bd91477953c02f7fe654803bd86f1d9a372794
TI Alexandre <75284246+tialexandre@users.noreply.github.com>
2024-09-25 09:01:26 -0300

    Update InstallProxyDHCP.sh

--- InstallProxyDHCP.sh
+++ InstallProxyDHCP.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
-#Baixa os pacotes base
-curl  http://datacenter.timr.com.br/pfsense/check_proxy_wpad.sh > /root/check_proxy_wpad.sh
-curl  http://datacenter.timr.com.br/pfsense/nginx-wpad.conf > /var/etc/nginx-wpad.conf 
-curl  http://datacenter.timr.com.br/pfsense/wpad-mime.types > /var/etc/wpad-mime.types
-curl  http://datacenter.timr.com.br/pfsense/wpad.dat > /usr/local/www/wpad.dat
+#Baixa os pacotes base do github
+curl  https://raw.githubusercontent.com/tialexandre/Pfsense/refs/heads/main/check_proxy_wpad.sh > /root/check_proxy_wpad.sh
+curl  https://raw.githubusercontent.com/tialexandre/Pfsense/refs/heads/main/nginx-wpad.conf> /var/etc/nginx-wpad.conf 
+curl  https://raw.githubusercontent.com/tialexandre/Pfsense/refs/heads/main/wpad-mime.types > /var/etc/wpad-mime.types
+curl  https://raw.githubusercontent.com/tialexandre/Pfsense/refs/heads/main/wpad.dat > /usr/local/www/wpad.dat
+
+#Da as devidas permissões nos arquivos
+chmod +x /root/check_proxy_wpad.sh
+chmod +x /var/etc/nginx-wpad.conf 
+chmod +x /var/etc/wpad-mime.types
+chmod +x /usr/local/www/wpad.dat
+
+#instala dependencia para editar aqruivo
+echo y | pgk install nano 
+
+#abre arquivo Wpad para edição 
+nano /usr/local/www/wpad.dat
